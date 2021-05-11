9dee8c88439ca4e934e64d101c8d011c727bce63
Simon C. Dockweiler <scdockweiler@gmail.com>
2021-05-11 10:36:01 +0200

    Tilføjede key filer

--- Code/Coffe_Vending_Machine/key.h
+++ Code/Coffe_Vending_Machine/key.h
@@ -1,24 +1,24 @@
 /*****************************************************************************
-* University of Southern Denmark
-* Embedded Programming (EMP)
-*
-* MODULENAME.: key.h
-*
-* PROJECT....: EMP
-*
-* DESCRIPTION: Test.
-*
-* Change Log:
-******************************************************************************
-* Date    Id    Change
-* YYMMDD
-* --------------------
-* 150321  MoH   Module created.
-*
-*****************************************************************************/
+ * University of Southern Denmark
+ * Embedded Programming (EMP)
+ *
+ * MODULENAME.: key.h
+ *
+ * PROJECT....: EMP
+ *
+ * DESCRIPTION: Test.
+ *
+ * Change Log:
+ ******************************************************************************
+ * Date    Id    Change
+ * YYMMDD
+ * --------------------
+ * 150321  MoH   Module created.
+ *
+ *****************************************************************************/
 
 #ifndef _KEY_H
-  #define _KEY_H
+#define _KEY_H
 
 INT8U get_keyboard();
 void key_task();
