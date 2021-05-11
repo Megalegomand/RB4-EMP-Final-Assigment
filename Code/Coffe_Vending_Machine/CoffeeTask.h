adff61e3bcf5e1fc9b00d6cabeaaa6fa359f4f0a
Rasmus <rasmc19@student.sdu.dk>
2021-05-11 10:32:46 +0200

    Added CoffeeTask

---
+++ Code/Coffe_Vending_Machine/CoffeeTask.h
@@ -0,0 +1,35 @@
+/*****************************************************************************
+* University of Southern Denmark
+* Embedded C Programming (ECP)
+*
+* MODULENAME.: CoffeeTask.h
+*
+* PROJECT: FinalAssignment - Coffe_Vending_Machine
+*
+* DESCRIPTION:
+*
+* Date of creation or change:
+******************************************************************************
+* Month     Day     Year    Change
+* 05        08      21
+* --------------------
+* 090215  MoH   Module created.
+*
+*****************************************************************************/
+#ifndef COFFEETASK_H_
+#define COFFEETASK_H_
+
+/***************************** Include files *******************************/
+/*****************************    Defines    *******************************/
+/*****************************   Constants   *******************************/
+/*****************************   Functions   *******************************/
+
+
+
+void Brewing (void);
+
+void Coffee_Task(void *pvParameters);
+
+
+
+#endif /* COFFEETASK_H_ */
